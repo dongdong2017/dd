984627d1ed5b0c2da465319ace445b254f9c5bc6
xumenglin <xumenglin@lonsid.cn>
2018-04-09 13:47:16 +0800

    异步

---
+++ Task/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tasktest
+{
+    class Program
+    {
+      
+        public async Task ImportAccId(string accName)
+        {
+            await Task.Delay(10000);
+            int _int = await add(accName); 
+        }
+
+        private Task<int> add(string accName)
+        {
+           if(accName == "aa")
+            {
+             
+                return 1231;
+            }
+            else
+            {
+                return 222;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string accName = "aa";
+            var accountid = Task.Run(async () =>
+            {
+                return await ImportAccId(accName);
+            });
+
+            Console.ReadLine( accountid.Result);
+
+        }
+    }
+}
